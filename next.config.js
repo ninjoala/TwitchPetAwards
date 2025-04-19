a730e912e07b4bfbdf7072def117f277ab120af3
Nicholas Dobos <ndobos95@gmail.com>
2025-04-18 22:59:44 -0400

    trying to fix a thing

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ const nextConfig = {
         headers: [
           {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' http://localhost:* https://localhost:* https://*.twitch.tv https://*.twitch.tech https://*.twitchcdn.net;"
+            value: "frame-ancestors 'self' http://*.traefik.me https://*.traefik.me http://localhost:* https://localhost:* https://*.twitch.tv https://*.twitch.tech https://*.twitchcdn.net;"
           }
         ],
       },
