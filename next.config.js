fff8ba16985fc27d253d912d18f5e1a35af0e10b
Nicholas Dobos <ndobos95@gmail.com>
2025-04-18 22:16:00 -0400

    fix cors

--- next.config.js
+++ next.config.js
@@ -4,6 +4,19 @@ const nextConfig = {
   images: {
     domains: ['uploadthing.com', 'utfs.io'], // Allow Uploadthing domains for images
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' http://localhost:* https://localhost:* https://*.twitch.tv https://*.twitch.tech https://*.twitchcdn.net;"
+          }
+        ],
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig 
